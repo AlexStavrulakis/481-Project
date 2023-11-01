4417fcaca6179c9d4a288c493e4af4f0a11539a3
Alexanthros Stavrulakis <as0318@mix.wvu.edu>
2023-11-01 16:20:21 -0400

    Initial Commit

---
+++ 481 Project/_81_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _81_ProjectApp.swift
+//  481 Project
+//
+//  Created by Alex Stavrulakis on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct _81_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
